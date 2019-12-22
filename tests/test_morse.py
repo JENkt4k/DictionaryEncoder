dca47820b7495a8378f074f735b037a563eee89a
James Nelson <james.e.nelson@gmail.com>
2019-12-21 22:01:48 -0600

    fix scope

--- tests/test_morse.py
+++ tests/test_morse.py
@@ -9,22 +9,22 @@ class TestCases(unittest.TestCase):
   def test_case_code_morse(self):
     message = "abbcc ccaabb"
     expected = "... --..-- --..-- etc. etc.|etc. etc. ... ... --..-- --..--"
-    self.assertEqual(expected, code_morse(message))
+    self.assertEqual(expected, morse.code_morse(message))
    
   def test_case_generic_encode(self):
       message = "abbcc ccaabb"
       expected = "... --..-- --..-- etc. etc.|etc. etc. ... ... --..-- --..--"
-      self.assertEqual(expected, encode_message(message, MORSE_DICT))
+      self.assertEqual(expected, morse.encode_message(message, morse.MORSE_DICT))
 
   def test_case_encode(self):
       message = "abbcc ccaabb"
       expected = "... --..-- --..-- etc. etc.|etc. etc. ... ... --..-- --..--"
-      self.assertEqual(expected, encode_morse(message))
+      self.assertEqual(expected, morse.encode_morse(message))
 
   def test_case_decode(self):
       expected = "abbcc ccaabb"
       message = "... --..-- --..-- etc. etc.|etc. etc. ... ... --..-- --..--"
-      self.assertEqual(expected, decode_morse(message))
+      self.assertEqual(expected, morse.decode_morse(message))
 
 if __name__ == "__main__":
     unittest.main()
