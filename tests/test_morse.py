292b851bd4da6146e266c9c1cd31da38bacaf00b
James Nelson <james.e.nelson@gmail.com>
2019-12-22 00:02:32 -0600

    remove code smell

--- tests/test_morse.py
+++ tests/test_morse.py
@@ -2,7 +2,6 @@ import unittest
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-#from morse import * #code_morse
 from DictionaryEncoder import morse
 
 class TestCases(unittest.TestCase):
