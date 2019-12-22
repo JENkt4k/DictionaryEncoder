2c4c8b9bb2787e80b0f129fcdbde1a5ecb8ced13
James Nelson <james.e.nelson@gmail.com>
2019-12-21 21:36:47 -0600

    update path

--- tests/test_DictionaryEncoder.py
+++ tests/test_DictionaryEncoder.py
@@ -1,6 +1,8 @@
 import unittest
+import os
 import sys
-sys.path.append('../DictionaryEncoder/')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from DictionaryEncoder import DictionaryEncoder
 
 
--- tests/test_MorseCodeDictionaryEncoder.py
+++ tests/test_MorseCodeDictionaryEncoder.py
@@ -1,6 +1,8 @@
 import unittest
+import os
 import sys
-sys.path.append('../DictionaryEncoder/')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from MorseCodeDictionaryEncoder import MorseCodeDictionaryEncoder
 
 class TestCases(unittest.TestCase):
